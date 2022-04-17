7e76b1e9556c1084830445dcb14e0fd4a335f8bb
MrYu-head <2537011422@qq.com>
2022-04-17 13:41:08 +0800

    第二次更新:2022.4.17 13:40

--- mongodb/user.js
+++ mongodb/user.js
@@ -1,26 +1,11 @@
-const mongoose = require("mongoose")
-
-// 1.连接数据库服务，创建数据库manshan
-mongoose
-    .connect("mongodb://localhost:27017/manshan")
-    .then(()=>{console.log("数据库连接成功。");})
-    .catch(()=>{console.log("数据库连接失败。");})
-
-// 2.建表,通过new去建表
-let Schema = mongoose.Schema
-let userSchema = new Schema({
-    // 用户表只有手机号和密码，没有用户名
-    phoneNumber : {type:Number,require:true},
-    password : {type:String,require:true},
-})
+var mongoose = require("mongoose");  //  顶会议用户组件
+var Schema = mongoose.Schema;    //  创建模型
+var userScheMa = new Schema({
+    phone: Number,
+    password: String
+}); //  定义了一个新的模型，但是此模式还未和users集合有关联
+exports.user = mongoose.model('user', userScheMa); //  与user集合关联
 
 
-// 3.规则创建好后就建表
-let user = mongoose.model("user",userSchema)
-// 通过建表之后返回的表对象进行各种数据的增删改查操作
-user.create({
-    phoneNumber : phoneNumber
-})
 
-module.exports = user
 
